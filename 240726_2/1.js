8ef84b365183a22cd760121943791146d7474b33
heerokj <jungheerok@gmail.com>
2024-07-26 16:41:25 +0900

    "2주차 과제"

--- 240726_2/1.js
+++ 240726_2/1.js
@@ -11,20 +11,15 @@ console.log(arr1); // [10, 2, 3] 출력
 console.log(arr1 === arr2); // true 출력 =>  같은 주소값을 참조하여 true
 
 
-var copyObject = function (object){
-  var result2 = {}
-  for(i in object){
-    result2[i] = object[i]
-  }
-  return result2
-}
-
 // 2.
 const obj1 = {a: 1, b: 2, c: 3};
-const obj2 = copyObject(obj1); // obj1을 얕은 복사하는 코드로 작성해주세요
+const obj2 = obj1; // obj1을 얕은 복사하는 코드로 작성해주세요
 
 /* obj2에 d를 추가하는 코드를 작성해주세요. d는 4의 값을 가집니다. */
 obj2.d = 4;
 /* 해당값 obj1, obj2의 값을 출력해주세요 */
 console.log(obj1);
 console.log(obj2);
+
+
+
